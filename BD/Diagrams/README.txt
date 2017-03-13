dcfbe965a2a4ff1d42fc2ba1e524aaa75ad1f453
Turcu Nicusor <nicusor.turcu@info.uaic.ro>
2017-03-13 08:28:24 +0200

    Uploaded part 1

---
+++ BD/Diagrams/README.txt
@@ -0,0 +1,2 @@
+BitChess.vpp - este un proiect realizat in Visual Paradigm.
+Am exportat png si pdf, in caz ca nu aveti instalat pe masina acesta aplicatie.
--- BD/Script Creare/BitChess_creation_script.sql
+++ BD/Script Creare/BitChess_creation_script.sql
@@ -36,12 +36,12 @@ DROP TABLE USERS CASCADE CONSTRAINTS;
 
 -- TABLES CREATION
 CREATE TABLE USER_STATUS (
-  id          number(10, 0) NOT NULL, 
+  id          number(10) NOT NULL, 
   description varchar2(255) NOT NULL, 
   PRIMARY KEY (id));
   
 CREATE TABLE OPENINGS (
-  id        number(10) NOT NULL, 
+  id        number(10, 0) NOT NULL, 
   name      varchar2(255) NOT NULL, 
   movements varchar2(1999) NOT NULL, 
   PRIMARY KEY (id));
@@ -51,8 +51,8 @@ CREATE TABLE GAMES (
   movements        varchar2(1999) NOT NULL, 
   game_result      char(1) NOT NULL, 
   started_at       timestamp(6) NOT NULL, 
-  first_player_id  number(10, 0) NOT NULL, 
-  second_player_id number(10, 0) NOT NULL, 
+  first_player_id  number(10) NOT NULL, 
+  second_player_id number(10) NOT NULL, 
   PRIMARY KEY (id));
   
 CREATE TABLE STATUS (
@@ -65,7 +65,7 @@ CREATE TABLE POST (
   content    varchar2(1023) NOT NULL, 
   created_at timestamp(6) NOT NULL, 
   thread_id  number(10, 0) NOT NULL, 
-  user_id    number(10, 0) NOT NULL, 
+  user_id    number(10) NOT NULL, 
   status_id  number(10, 0) NOT NULL, 
   PRIMARY KEY (id));
   
@@ -74,7 +74,7 @@ CREATE TABLE THREAD (
   name        varchar2(255) NOT NULL, 
   description varchar2(1023) NOT NULL, 
   created_at  timestamp(6) NOT NULL, 
-  user_id     number(10, 0) NOT NULL, 
+  user_id     number(10) NOT NULL, 
   status_id   number(10, 0) NOT NULL, 
   category_id number(10, 0) NOT NULL, 
   PRIMARY KEY (id));
@@ -87,19 +87,19 @@ CREATE TABLE CATEGORY (
   PRIMARY KEY (id));
   
 CREATE TABLE CLUBS (
-  id     number(10, 0) NOT NULL, 
+  id     number(10) NOT NULL, 
   name   varchar2(255) NOT NULL, 
-  rating number(10) NOT NULL, 
+  rating number(10, 0) NOT NULL, 
   PRIMARY KEY (id));
   
 CREATE TABLE FRIENDS (
-  first_user_id  number(10, 0) NOT NULL, 
-  second_user_id number(10, 0) NOT NULL, 
+  first_user_id  number(10) NOT NULL, 
+  second_user_id number(10) NOT NULL, 
   PRIMARY KEY (first_user_id, 
   second_user_id));
   
 CREATE TABLE USERS (
-  id         number(10, 0) NOT NULL, 
+  id         number(10) NOT NULL, 
   name       varchar2(255) NOT NULL, 
   email      varchar2(255) NOT NULL, 
   nickname   varchar2(255) NOT NULL, 
@@ -108,11 +108,11 @@ CREATE TABLE USERS (
   looses     number(10, 0) NOT NULL, 
   draws      number(10, 0) NOT NULL, 
   created_at timestamp(6) NOT NULL, 
-  status_id  number(10, 0) NOT NULL, 
-  club_id    number(10, 0), 
+  status_id  number(10) NOT NULL, 
+  club_id    number(10), 
   PRIMARY KEY (id));
-  
-ALTER TABLE USERS ADD CONSTRAINT USER_STATUS_ID_FK FOREIGN KEY (status_id) REFERENCES USER_STATUS (id);
+
+-- TABLES FOREIGN KEY CONSTRAINT
 ALTER TABLE POST ADD CONSTRAINT POST_THREAD_ID_FK FOREIGN KEY (thread_id) REFERENCES THREAD (id);
 ALTER TABLE THREAD ADD CONSTRAINT THREAD_STATUS_ID_FK FOREIGN KEY (status_id) REFERENCES STATUS (id);
 ALTER TABLE POST ADD CONSTRAINT POST_USER_ID_FK FOREIGN KEY (user_id) REFERENCES USERS (id);
@@ -124,14 +124,35 @@ ALTER TABLE GAMES ADD CONSTRAINT GAMES_FIRST_PLAYER_ID_FK FOREIGN KEY (first_pla
 ALTER TABLE POST ADD CONSTRAINT POST_STATUS_ID_FK FOREIGN KEY (status_id) REFERENCES STATUS (id);
 ALTER TABLE FRIENDS ADD CONSTRAINT FRIENDS_FIRST_USER_ID_FK FOREIGN KEY (second_user_id) REFERENCES USERS (id);
 ALTER TABLE FRIENDS ADD CONSTRAINT FRIENDS_SECOND_USER_ID_FK FOREIGN KEY (first_user_id) REFERENCES USERS (id);
+ALTER TABLE USERS ADD CONSTRAINT USER_STATUS_ID_FK FOREIGN KEY (status_id) REFERENCES USER_STATUS (id);
 
+-- TABLES INDEXES
+CREATE INDEX USER_STATUS_id 
+  ON USER_STATUS (id);
+CREATE INDEX OPENINGS_id 
+  ON OPENINGS (id);
+CREATE INDEX GAMES_id 
+  ON GAMES (id);
+CREATE INDEX STATUS_id 
+  ON STATUS (id);
+CREATE INDEX POST_id 
+  ON POST (id);
+CREATE INDEX THREAD_id 
+  ON THREAD (id);
+CREATE INDEX CATEGORY_id 
+  ON CATEGORY (id);
+CREATE INDEX CLUBS_id 
+  ON CLUBS (id);
+CREATE INDEX USERS_id 
+  ON USERS (id);
 -- 10K entrie in some tables
+
 BEGIN
   -- add entries for user_status table
   DECLARE
      v_description user_status.description%type;
   BEGIN
-    FOR v_cont in 1..100 LOOP
+    FOR v_cont in 1..10000 LOOP
       v_description :=  initcap(DBMS_RANDOM.string('l', DBMS_RANDOM.value(10,20)));
       INSERT INTO USER_STATUS(id, description) VALUES (USER_STATUS_ID.NEXTVAL, v_description);
     END LOOP;
@@ -150,7 +171,7 @@ BEGIN
   BEGIN
     v_wins:= USER_ID.NEXTVAL; -- doar ca sa pot apela mai jos, inainte sa fac vreun insert USER_ID.currval
   
-    FOR v_cont in 1..100 LOOP
+    FOR v_cont in 1..10000 LOOP
       v_name := initcap(DBMS_RANDOM.string('l', dbms_random.value(10,20)));
       v_email := v_name||'@info.uaic.ro';
       v_nickname := DBMS_RANDOM.string('l', DBMS_RANDOM.value(5,10));
@@ -168,7 +189,7 @@ BEGIN
     v_first_user_id friends.first_user_id%type;
     v_second_user_id friends.second_user_id%type;
   BEGIN
-    FOR v_cont in 1..50 LOOP
+    FOR v_cont in 1..1000 LOOP
       v_first_user_id := TRUNC(DBMS_RANDOM.value(1,USER_ID.currval));
       v_second_user_id :=  TRUNC(DBMS_RANDOM.VALUE(1,USER_ID.currval));
       INSERT INTO FRIENDS(first_user_id, second_user_id) VALUES (v_first_user_id,v_second_user_id);

0c5e2535c836cecb7c4a54dd88fdeb26ff596b28
Turcu Nicusor <nicusor.turcu@info.uaic.ro>
2017-04-02 14:45:27 +0300

    Added all chess pieces 3D models.

--- WebGl/FragmentShader.glsl
+++ WebGl/FragmentShader.glsl
@@ -3,8 +3,18 @@
 precision mediump float;
 
 varying vec3 fragNormal;
+varying vec3 fragPosition;
 
 void main()
 {
-	gl_FragColor = vec4(fragNormal, 1.0);
+	vec3 modelColor = vec3(1.0, 0.0, 0.0);
+	vec3 ambientColor = vec3(0.2, 0.2, 0.2);
+	vec3 diffuseColor = vec3(0.9, 0.9, 0.9);
+	vec3 direction = vec3(0.1, -0.5, 1.0);
+
+	// Diffuse component
+	float diffValue = max(dot(fragNormal, direction), 0.0);
+	vec3 diffuse = diffValue * diffuseColor;
+
+	gl_FragColor = vec4((ambientColor + diffuse) * modelColor, 1.0);
 }
--- WebGl/main.js
+++ WebGl/main.js
@@ -8,11 +8,26 @@ function InitWebGl() {
 	}
 	
 	// create model
-	var teapotModel = new Model(gl);
-	teapotModel.init(downloadManager.getData('teapot.obj'));
+	var teapot = new Model(gl);
+	teapot.init(downloadManager.getData('models/teapot.obj'));
 
-	var teapotModel2 = new Model(gl);
-	teapotModel2.init(downloadManager.getData('cube.obj'));
+	var pawn = new Model(gl);
+	pawn.init(downloadManager.getData('models/pawn.obj'));
+
+	var king = new Model(gl);
+	king.init(downloadManager.getData('models/king.obj'));
+
+	var queen = new Model(gl);
+	queen.init(downloadManager.getData('models/queen.obj'));
+
+	var bishop = new Model(gl);
+	bishop.init(downloadManager.getData('models/bishop.obj'));
+
+	var knight = new Model(gl);
+	knight.init(downloadManager.getData('models/knight.obj'));
+
+	var rook = new Model(gl);
+	rook.init(downloadManager.getData('models/rook.obj'));
 
 	// creating shader
 	var defaultShader = new Shader(gl);
@@ -22,24 +37,32 @@ function InitWebGl() {
 	var projMatrix = new Float32Array(16);
 	var viewMatrix = new Float32Array(16);
 	
-	mat4.lookAt(viewMatrix,[0,0,5],[0.0,0,0],[0,1,0]); // camera position, lookAt, up
+	mat4.lookAt(viewMatrix,[0,2,10],[0.0,0,0],[0,1,0]); // camera position, lookAt, up
 	mat4.perspective(projMatrix,glMatrix.toRadian(45), canvas.width/canvas.height, 0.1, 100.0);
 
 	gl.uniformMatrix4fv(defaultShader.getMatViewUniformLocation(), gl.FALSE, viewMatrix);
 	gl.uniformMatrix4fv(defaultShader.getMatProjectionUniformLocation(), gl.FALSE, projMatrix);
 
-
-	teapotModel.setPosition(1.2,0,0);
-	teapotModel2.setPosition(-0.3,0,0);
+	teapot.setPosition(6, 0, 0);
+	pawn.setPosition(4.5, 0, 0);
+	king.setPosition(3, 0, 0);
+	queen.setPosition(1.5, 0, 0);
+	bishop.setPosition(0, 0, 0);
+	knight.setPosition(-1.5, 0, 0);
+	rook.setPosition(-3, 0, 0);
 	//main render loop
 	gl.enable(gl.DEPTH_TEST);
 	var loop = function() {
 		gl.clearColor(0.9, 0.9, 0.9, 1.0);
 		gl.clear(gl.DEPTH_BUFFER_BIT | gl.COLOR_BUFFER_BIT);
 
-		teapotModel.render(defaultShader);
-		teapotModel2.render(defaultShader);
-
+		teapot.render(defaultShader);
+		pawn.render(defaultShader);
+		king.render(defaultShader);
+		queen.render(defaultShader);
+		bishop.render(defaultShader);
+		knight.render(defaultShader);
+		rook.render(defaultShader);
 		requestAnimationFrame(loop);
 	}
 	requestAnimationFrame(loop);
@@ -53,7 +76,12 @@ function OnLoad() {
 	downloadManager = new DownloadManager();
 	downloadManager.addResourceToQueue("VertexShader.glsl");
 	downloadManager.addResourceToQueue("FragmentShader.glsl");
-	downloadManager.addResourceToQueue("cube.obj");
-	downloadManager.addResourceToQueue("teapot.obj");
+	downloadManager.addResourceToQueue("models/pawn.obj");
+	downloadManager.addResourceToQueue("models/king.obj");
+	downloadManager.addResourceToQueue("models/queen.obj");
+	downloadManager.addResourceToQueue("models/bishop.obj");
+	downloadManager.addResourceToQueue("models/knight.obj");
+	downloadManager.addResourceToQueue("models/rook.obj");
+	downloadManager.addResourceToQueue("models/teapot.obj");
 	downloadManager.beginDownload(InitWebGl);
 };

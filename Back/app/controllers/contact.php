2295a26bdc42f899eb1ea13bf22e0e90d4066abc
Djok216 <motroi.valeriu@yahoo.com>
2017-04-11 12:23:15 +0300

    MVC in PHP

---
+++ Back/app/controllers/contact.php
@@ -0,0 +1,11 @@
+<?php
+
+class Contact extends Controller {
+    public function index() {
+        echo 'contact index';
+    }
+
+    public function phone() {
+        echo 'contact phone';
+    }
+}

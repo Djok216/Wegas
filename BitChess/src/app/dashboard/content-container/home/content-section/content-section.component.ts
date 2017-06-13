a9a5eee59e2da95052f8b5ae428e78d75d4684be
Djok216 <motroi.valeriu@yahoo.com>
2017-06-13 14:53:20 +0300

    home live chess lichess

--- BitChess/src/app/dashboard/content-container/home/content-section/content-section.component.ts
+++ BitChess/src/app/dashboard/content-container/home/content-section/content-section.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import './liveChess.js';
+
+declare const LiveChess: any;
 
 @Component({
   selector: 'app-content-section',
   templateUrl: 'content-section.component.html',
   styleUrls: ['content-section.component.css']
 })
+
 export class ContentSectionComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
   }
 
+  ngOnInit() {
+    const game = new LiveChess();
+  }
 }
